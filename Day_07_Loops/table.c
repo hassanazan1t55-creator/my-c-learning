1e4f94d4af6a0bba185e41003bda769920c01c52
hassanazan1t55-creator <hassanazan1t55@gmail.com>
2026-07-01 03:46:56 +0500

    Add multiplication table program for 2
    Implement a program to print the multiplication table of 2.

---
+++ Day_07_Loops/table.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int counting;
+
+    // loop run 10 times for table
+    // (Table ke liye loop 10 dafa chalega)
+    for(counting = 1; counting <= 10; counting++) {
+        
+        // print table design and do math
+        // (Table ka design banana aur math karna)
+        printf("2 x %d = %d\n", counting, 2 * counting);
+    }
+
+    return 0;
+}
