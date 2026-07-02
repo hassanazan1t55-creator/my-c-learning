8a9b95be2a91f6a4cf6ad0711b4464094761199a
Hassan Azan <hassanazan1t55@gmail.com>
2026-07-03 04:34:42 +0500

    Add lucky_numbers.c to demonstrate array usage

---
+++ Day_13_Arrays/lucky_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // make array with 4 boxes
+    // (4 dabbay wala array banaya)
+    int lucky_number[4] = {7, 11, 20, 99};
+ 
+    // print box number 3 because counting start from 0
+    // (Teesra dabba print karna kyunki ginti 0 se shuru hoti hai)
+    printf("Ye ly Sir 3 daby ki value print ho rahi hai: %d\n", lucky_number[2]);
+ 
+    return 0;
+}
