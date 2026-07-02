2edbc7051b9ec58c9124a665bea8bbf5c6d13f10
Hassan Azan <hassanazan1t55@gmail.com>
2026-07-03 04:39:38 +0500

    Add C program to display favorite programming language

---
+++ Day_15_Strings/fav_lang.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // make char array for store text name
+    // (Text name save karne ke liye char array banaya)
+    char fav_language[] = "C Language";
+ 
+    // print text using %s for string
+    // (String ke liye %s use kar ke text print karna)
+    printf("My favorite programming Language is: %s\n", fav_language);
+ 
+    return 0;
+}
