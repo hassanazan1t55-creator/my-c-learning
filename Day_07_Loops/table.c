893cabf51e0dc467d82f20c8f5da21133acbc7f8
Hassan Azan <hassanazan1t55@gmail.com>
2026-07-09 03:37:23 +0500

    Clean up formatting in table.c

--- Day_07_Loops/table.c
+++ Day_07_Loops/table.c
@@ -2,19 +2,15 @@
 
 int main() {
   // Initialize a variable to store a User Input.
-
   int num;
-
+  
   // Prompt the user to enter a number for which they want to print the
   // multiplication table.
-
   printf("Enter a number to print its multiplication table: ");
-
   scanf("%d", &num);
 
   for (int i = 1; i <= 10; i++) {
     printf("%d x %d = %d\n", num, i, num * i);
   }
-
   return 0;
 }
