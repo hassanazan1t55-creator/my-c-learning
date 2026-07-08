598e693ca2f3422b544edfb6db0f9f8aabcbb3e6
Hassan Azan <hassanazan1t55@gmail.com>
2026-07-09 03:07:26 +0500

    Refactor ATM PIN verification logic
    Refactored ATM PIN verifier to check secret key before prompting for PIN. Removed nested do-while loop for PIN input.

---
+++ Day_09_Do_While_Loop/atm_pin_verifier.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+  int secret_key = 12345;   // Variable to store the secret key.
+  int user_input;   // Variable to store user input.
+  
+  // Loop until the user enters the correct secret key.
+  do {
+  // prompt the user to enter a key.
+  printf("Enter the secret key: ");
+  scanf("%d", &user_input);
+
+  // Check if the user input matches the secret key.
+  if (user_input == secret_key) {
+    printf("Access Granted!\n");
+  } else {
+    printf("Access Denied! Try again.\n");
+  
+  }
+  } while (user_input != secret_key); // Continue Looping until the correct key is entered.
+  return 0;  // Return 0 to indicate successful execution.
+  
+}
