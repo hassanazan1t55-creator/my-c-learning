ce820a87b5072a2bc4cbb353aa56c6610de97c80
Hassan Azan <hassanazan1t55@gmail.com>
2026-07-11 01:25:48 +0500

    Add intro_to_arrays.c for array demonstration

---
+++ Day_13_Arrays/intro_to_arrays.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  // Declaring an array.
+  int marks[3] = {85, 92, 78};  // 3 elements.
+
+  // Accessing elements of an array.
+  printf("Marks of student 1: %d\n", marks[0]);
+  printf("Marks of student 2: %d\n", marks[1]);
+  printf("Marks of student 3: %d\n", marks[2]);
+  return 0;
+}
