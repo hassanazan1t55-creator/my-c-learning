49b622027b8222876d121a993d2cfbe3f5068e7f
Hassan Azan <hassanazan1t55@gmail.com>
2026-07-10 06:13:49 +0500

    Remove emojis from game menu messages

--- Day_09_Do_While_Loop/game_menu_system.c
+++ Day_09_Do_While_Loop/game_menu_system.c
@@ -12,10 +12,10 @@ int main() {
 
         // Game logic (if-else) MUST be inside the do block
         if (choice == 1) {
-            printf("Game Started! 🎮\n");
+            printf("Game Started!\n");
         } 
         else if (choice == 2) {
-            printf("Exiting Game. Goodbye! 👋\n");
+            printf("Exiting Game. Goodbye!\n");
         } 
         else {
             printf("Invalid choice. Please Enter 1 to Start Game or 2 to Exit Game.\n");
