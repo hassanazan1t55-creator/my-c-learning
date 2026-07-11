409009e9843a50e988143644643eff2ba5e8c971
Hassan Azan <hassanazan1t55@gmail.com>
2026-07-12 03:03:38 +0500

    Refactor comments for clarity in employee insertion

--- Day_37_Insert_At_Position/employee_id_insertion.c
+++ Day_37_Insert_At_Position/employee_id_insertion.c
@@ -8,7 +8,7 @@ struct EmployeeNode {
 };
 
 int main() {
-    // 1. Setup initial employee list: 101 -> 103
+    // Setup initial employee list: 101 -> 103
     struct EmployeeNode *emp1 = (struct EmployeeNode *)malloc(sizeof(struct EmployeeNode));
     struct EmployeeNode *emp3 = (struct EmployeeNode *)malloc(sizeof(struct EmployeeNode));
 
@@ -18,11 +18,11 @@ int main() {
     emp3->emp_id = 103;
     emp3->next = NULL;
 
-    // 2. Create new employee 102 to insert at position 2
+    // Create new employee 102 to insert at position 2
     struct EmployeeNode *emp2 = (struct EmployeeNode *)malloc(sizeof(struct EmployeeNode));
     emp2->emp_id = 102;
 
-    // 3. Move to position 1
+    // Move to position 1
     struct EmployeeNode *temp = emp1;
     int target_position = 2;
 
@@ -30,11 +30,11 @@ int main() {
         temp = temp->next;
     }
 
-    // 4. Link 102 in the middle
+    // Link 102 in the middle
     emp2->next = temp->next; // Link 102 to 103
     temp->next = emp2;       // Link 101 to 102
 
-    // 5. Print the employee list
+    // Print the employee list
     struct EmployeeNode *printPtr = emp1;
     printf("--- EMPLOYEE ID LIST ---\n\n");
     while (printPtr != NULL) {
