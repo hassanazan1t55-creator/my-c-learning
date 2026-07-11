8de28798737d399ea498fc1afa94661fa6b28dad
Hassan Azan <hassanazan1t55@gmail.com>
2026-07-12 03:22:54 +0500

    Add intro_to_strings.c for string manipulation example

---
+++ Day_15_Strings/intro_to_strings.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  char name[] = "Azan Hassan";  // Array of characters.
+
+  // Displaying Output.
+  printf("Welcome Sir, %s\n", name);
+
+  printf("Sir Your Name First Letter is: %c\n", name[0]);
+  printf("Sir Your Name Second Letter is: %c\n", name[1]);
+
+  return 0;
+}
