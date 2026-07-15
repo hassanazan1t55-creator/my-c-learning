c11a32c4bdec1772c633b05b3542bc2051cca652
Hassan Azan <hassanazan1t55@gmail.com>
2026-07-16 04:57:19 +0500

    Update and rename bitwise_xor_not.c to 01_bitwise_basics.c

---
+++ Day_26_Bitwise_Operators/01_bitwise_basics.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int a = 5;  // Binary: 0101
+  int b = 6;  // Binary: 0110
+
+  // Displaying Bitwise Operations of a and b.
+  printf("Bitwise AND (a & b) = %d\n", a & b);
+  printf("Bitwise OR (a | b) = %d\n", a | b);
+  printf("Bitwise XOR (a ^ b) = %d\n", a ^ b);
+  printf("Bitwise NOT (~a) = %d\n", ~a);
+
+  return 0;
+}
