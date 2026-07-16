a9b2a4961cdc5cad9f6e831397114419165be796
Hassan Azan <hassanazan1t55@gmail.com>
2026-07-17 03:47:45 +0500

    Add bitwise shift operations in 01_shift_math.c

---
+++ Day_27_Bitwise_Shifts/01_shift_math.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int number = 15;  // Binary: 00001111
+
+  // Left shift by 3 bits.
+  int left_shift_res = number << 3;
+  printf("Original Number: %d\n", number);
+  printf("Left Shift by << 3 bits: %d\n", left_shift_res);
+
+  // Right shift by 2 bits.
+  int right_shift_res = left_shift_res >> 2;
+  printf("Right Shift by >> 2 bits: %d\n", right_shift_res);
+
+  return 0;
+}
