de7b2b7436648017165df13f1c198ffbb4165e9c
Hassan Azan <hassanazan1t55@gmail.com>
2026-07-21 20:03:46 +0500

    Refine comments in structure padding example
    Updated comments for clarity and consistency.

--- Day_30_Structure_Padding/01_padding_intro.c
+++ Day_30_Structure_Padding/01_padding_intro.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 struct Badge {
-  char id;     // Takes 1 byte
-  int points;  // Takes 4 bytes
+  char id;     // Takes 1 byte.
+  int points;  // Takes 4 bytes.
 };
 
 int main() {
   struct Badge player1;
 
-  // Mathematically it should be 1 + 4 = 5 bytes
-  // Let's see what the computer actually gives us!
+  // Mathematically it should be 1 + 4 = 5 bytes.
+  // Let's see what the computer actually gives us.
   printf("Actual size of structure in memory: %zu bytes\n", sizeof(player1));
 
   return 0;
