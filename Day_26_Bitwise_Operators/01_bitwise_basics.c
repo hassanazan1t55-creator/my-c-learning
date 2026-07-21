68c599cd46fcb0e69ee95e08e22faef26d0e1526
Hassan Azan <hassanazan1t55@gmail.com>
2026-07-21 20:07:10 +0500

    Fix comment formatting in bitwise_basics.c
    Updated comments to include periods for consistency.

--- Day_26_Bitwise_Operators/01_bitwise_basics.c
+++ Day_26_Bitwise_Operators/01_bitwise_basics.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  int a = 5;  // Binary: 0101
-  int b = 6;  // Binary: 0110
+  int a = 5;  // Binary: 0101.
+  int b = 6;  // Binary: 0110.
 
   // Displaying Bitwise Operations of a and b.
   printf("Bitwise AND (a & b) = %d\n", a & b);
