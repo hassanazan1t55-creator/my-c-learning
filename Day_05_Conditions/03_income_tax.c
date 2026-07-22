0223c6c75dbad8e23464f52da0f7d33f1b7bc49c
Hassan Azan <hassanazan1t55@gmail.com>
2026-07-22 19:15:00 +0500

    Add income tax calculation program
    This program calculates income tax based on user salary input, applying different tax rates for different salary brackets.

---
+++ Day_05_Conditions/03_income_tax.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+  // Declaring Variables.
+  int salary;
+  float total = 0.0;
+
+  // Input from user to enter salary.
+  printf("Enter your salary: ");
+  scanf("%d", &salary);
+
+  // Checking conditions and apply Tax.
+  if (salary > 100000) {
+    total = 2500 + (salary - 1e5) * 0.10;
+    printf("Total Tax: %.1f", total);
+  } else if (salary > 50000) {
+    total = (salary - 50000) * 0.05;
+    printf("Total Tax: %.1f", total);
+  } else {
+    printf("Total Tax: 0");
+  }
+
+  return 0;
+}
