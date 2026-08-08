03a750484534d281a3c258a7de5a65fa09157855
Hassan Azan <hassanazan1t55@gmail.com>
2026-08-08 08:47:54 +0500

    Add modulus divisibility check example
    Implement modulus operator to check divisibility by 97.

---
+++ Day_04_Operators/02_modulus_divisibility_check.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2344234;
+
+    // Modulus operator (%) returns the remainder of division
+    printf("The remainder of %d divided by 97 is: %d\n", a, a % 97);
+
+    // If output returns 0 = divisible
+    // Else = not divisible
+
+    return 0;
+}
