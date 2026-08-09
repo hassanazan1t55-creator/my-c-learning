b87491f6a72b0347fa808ed85c4e1cb6bccd9978
Hassan Azan <hassanazan1t55@gmail.com>
2026-08-09 05:07:25 +0500

    Create 04_ternary_operator.c

---
+++ Day_04_Operators/04_ternary_operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Syntax: condition ? expression_if_true : expression_if_false
+    int a = 35;
+    int b = 32;
+
+    a > b ? printf("A is greater\n") : printf("B is greater\n");
+
+    return 0;
+}
