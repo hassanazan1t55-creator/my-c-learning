7c6822143ea5f713918f3321263c852dc35df412
Hassan Azan <hassanazan1t55@gmail.com>
2026-08-10 06:06:26 +0500

    Add while loop to print birthday message

---
+++ Day_08_While_Loop/01_while_loop_basics.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+
+    // Print message 3 times using while loop
+    while (i < 3) {
+        printf("Happy Birthday!\n");
+        i++;
+    }
+
+    return 0;
+}
