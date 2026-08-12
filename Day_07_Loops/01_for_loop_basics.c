8e0e12ac6093ba665576764a406807875a583e9c
Hassan Azan <hassanazan1t55@gmail.com>
2026-08-12 06:24:22 +0500

    Create 01_for_loop_basics.c

---
+++ Day_07_Loops/01_for_loop_basics.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n = 6;
+
+    // Basic for loop to print numbers from 1 to n
+    for (int i = 1; i <= n; i++) {
+        printf("The value of i is: %d\n", i);
+    }
+
+    return 0;
+}
