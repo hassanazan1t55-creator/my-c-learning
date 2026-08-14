2ff93399d642123147c571038fbfaaa397acb4b7
Hassan Azan <hassanazan1t55@gmail.com>
2026-08-15 02:36:46 +0500

    Add prime number check using do-while loop

---
+++ Day_09_Do_While_Loop/02_prime_check_do_while.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n = 10;
+    int Not_prime = 0;
+
+    // 0 and 1 are not prime
+    if (n == 0 || n == 1) {
+        Not_prime = 1;
+    } 
+    // 2 is prime, no need to loop
+    else if (n == 2) {
+        Not_prime = 0;
+    } 
+    else {
+        int i = 2;
+        do {
+            if (n % i == 0) {
+                Not_prime = 1;
+                break; // Factor mil gaya, loop roko
+            }
+            i++;
+        } while (i < n);
+    }
+
+    if (Not_prime) {
+        printf("%d is not a prime number.\n", n);
+    } else {
+        printf("%d is a prime number.\n", n);
+    }
+
+    return 0;
+}
