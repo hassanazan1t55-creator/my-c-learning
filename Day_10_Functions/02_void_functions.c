e515523fb0317aeec16b06816a4d76755d6eeae2
Hassan Azan <hassanazan1t55@gmail.com>
2026-08-16 06:19:54 +0500

    Add void functions for greeting messages

---
+++ Day_10_Functions/02_void_functions.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Function prototypes
+void goodMorning();
+void goodAfternoon();
+void goodNight();
+
+// Function definitions
+void goodMorning() {
+    printf("Good Morning!\n");
+}
+
+void goodAfternoon() { 
+    printf("Good Afternoon!\n");
+}
+
+void goodNight() { 
+    printf("Good Night!\n"); 
+}
+
+int main() {
+    goodMorning();
+    goodAfternoon();
+    goodNight();
+    
+    return 0;
+}
